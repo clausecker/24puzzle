2010ad2c9d1100f37b842cb86301fcd63f08ec2c
Robert Clausecker <fuz@fuz.su>
2017-08-09 16:10:23 +0200

    Add genpdb command
    Doesn't seem to work properly yet.

--- Makefile
+++ Makefile
@@ -2,14 +2,17 @@ CC=clang
 CFLAGS=-msse4.2 -mpopcnt -O3 -Wall -Wno-missing-braces -I.
 
 OBJ=index.o puzzle.o tileset.o validation.o pdbgen.o
-BINARIES=test/indextest
+BINARIES=test/indextest cmd/genpdb.o
 
 all: $(OBJ)
 
 test/indextest: $(OBJ) test/indextest.o
 	$(CC) $(LDFLAGS) -o $@ $(OBJ) test/indextest.o
 
+cmd/genpdb: $(OBJ) cmd/genpdb.o
+	$(CC) $(LDFLAGS) -o $@ $(OBJ) cmd/genpdb.o
+
 clean:
-	rm -f *.o test/*.o $(BINARIES)
+	rm -f *.o test/*.o cmd/*.o $(BINARIES)
 
 .PHONY: all clean
---
+++ cmd/genpdb.c
@@ -0,0 +1,98 @@
+/* genpdb.c -- generate a PDB */
+
+#define _POSIX_C_SOURCE 200809L
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "tileset.h"
+#include "index.h"
+#include "pdb.h"
+
+static void
+usage(const char *argv0)
+{
+	fprintf(stderr, "Usage: %s [-f file] [-t tile,tile,...]\n", argv0);
+
+	exit(EXIT_FAILURE);
+}
+
+extern int
+main(int argc, char *argv[])
+{
+	tileset ts = 0x00000e7; /* 0 1 2 5 6 7 */
+	size_t count, size;
+	int optchar, tile;
+	const char *fname = NULL, *token;
+
+	patterndb pdb;
+	FILE *f = NULL;
+
+	while (optchar = getopt(argc, argv, "f:t:"), optchar != -1)
+		switch (optchar) {
+		case 'f':
+			fname = optarg;
+			break;
+
+		case 't':
+			ts = EMPTY_TILESET;
+			token = strtok(optarg,",");
+
+			while (token != NULL) {
+				tile = atoi(token);
+				if (tile < 0 || tile >= TILE_COUNT) {
+					fprintf(stderr, "Tile out of range: %s\n", token);
+					return (EXIT_FAILURE);
+				}
+
+				ts = tileset_add(ts, tile);
+				token = strtok(NULL,",");
+			}
+
+			break;
+
+		case '?':
+		case ':':
+			usage(argv[0]);
+		}
+
+	if (tileset_count(ts) >= 16) {
+		fprintf(stderr, "%d tiles are too many tiles. Up to 15 tiles allowed.\n",
+		    tileset_count(ts));
+		return (EXIT_FAILURE);
+	}
+
+	if (fname != NULL) {
+		f = fopen(fname, "wb");
+		if (f == NULL) {
+			perror("fopen");
+			return (EXIT_FAILURE);
+		}
+	}
+
+	size = search_space_size(ts);
+
+	pdb = malloc(size);
+	if (pdb == NULL) {
+		perror("malloc");
+		return (EXIT_FAILURE);
+	}
+
+	generate_patterndb(pdb, ts, stderr);
+
+	if (f != NULL) {
+		count = fwrite(pdb, 1, size, f);
+
+		if (count < size) {
+			if (ferror(f))
+				perror("fwrite");
+			else
+				fprintf(stderr, "End of file writing PDB.\n");
+
+			return (EXIT_FAILURE);
+		}
+	}
+
+	return (EXIT_SUCCESS);
+}
