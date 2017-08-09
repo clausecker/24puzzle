a8abdb17caa5a8fc2039bd4c78b968a2625838bb
Robert Clausecker <fuz@fuz.su>
2017-08-09 12:41:24 +0200

    tileset.c: Correct tileset_eqclass()
    The implementation was completely wrong before. Now it
    seems to make a bit of sense at least.

--- tileset.c
+++ tileset.c
@@ -4,18 +4,18 @@
 #include "tileset.h"
 
 /*
- * Given a tileset ts representing free positions on the grid and
- * a square number t set in ts, return a tileset representing
+ * Given a tileset cmap representing free positions on the grid and
+ * a square number t set in cmap, return a tileset representing
  * all squares reachable from t through members of ts.
  */
 static tileset
-tileset_flood(tileset ts, unsigned t)
+tileset_flood(tileset cmap, unsigned t)
 {
 	tileset r = tileset_add(EMPTY_TILESET, t), oldr;
 
 	do {
 		oldr = r;
-		r = ts & (r | r  << 5 | r << 1 | r >> 5 | r >> 1);
+		r = cmap & (r | r  << 5 | r << 1 | r >> 5 | r >> 1);
 	} while (oldr != r);
 
 	return (r);
@@ -42,11 +42,19 @@ tileset_map(tileset ts, const struct puzzle *p)
  * For a tileset ts which may or may not contain the zero tile
  * and a puzzle configuration p, compute a tileset representing
  * the squares we can move the zero tile to without disturbing
- * the non-zero tiles in ts.
+ * the non-zero tiles in ts.  If the zero tile is not in ts, it
+ * this is just the set of squares not occupied by tiles in ts
+ * as we cannot make assumptions about the position of the zero
+ * tile.
  */
 extern tileset
 tileset_eqclass(tileset ts, const struct puzzle *p)
 {
-	return (tileset_flood(tileset_map(tileset_remove(ts, 0), p), p->tiles[0]));
+	tileset cmap = tileset_complement(tileset_map(tileset_remove(ts, 0), p));
+
+	if (tileset_has(ts, 0))
+		return (tileset_flood(cmap, p->tiles[0]));
+	else
+		return (cmap);
 }
 
