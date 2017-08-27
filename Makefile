d1afafc8be34f3aa90edbe2cf951f3c5de42f1fe
Robert Clausecker <fuz@fuz.su>
2017-08-27 16:23:38 +0200

    cmd/pdbstats.c: Rewrite program to be independent of the index function

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC=clang
-CFLAGS=-march=native -mpopcnt -O3 -Wall -Wno-missing-braces -Wno-parentheses -I. -g
+CFLAGS=-march=native -O3 -Wall -Wno-missing-braces -Wno-parentheses -I. -g
 LDLIBS=-lpthread
 
 #OBJ=index.o puzzle.o tileset.o validation.o pdbgen.o pdbverify.o parallel.o \
@@ -7,7 +7,7 @@ LDLIBS=-lpthread
 #BINARIES=test/indextest test/tiletest cmd/genpdb cmd/verifypdb cmd/reducepdb \
 #	cmd/pdbstats util/rankgen test/ranktest
 OBJ=index.o puzzle.o tileset.o validation.o ranktbl.o rank.o
-BINARIES=test/indextest test/tiletest util/rankgen test/ranktest
+BINARIES=cmd/pdbstats test/indextest test/tiletest util/rankgen test/ranktest
 
 all: $(BINARIES) 24puzzle.a
 
--- cmd/pdbstats.c
+++ cmd/pdbstats.c
@@ -9,16 +9,51 @@
 
 #include <unistd.h>
 
-#include "tileset.h"
-#include "index.h"
 #include "pdb.h"
 
 static void
 usage(const char *argv0)
 {
-	fprintf(stderr, "Usage: %s [-t tile,tile,tile,...] -f file [-j nproc]\n", argv0);
+	fprintf(stderr, "Usage: %s pdbfile\n", argv0);
 
 	exit(EXIT_FAILURE);
+}
+
+/*
+ * Gather statistics about the PDB.  Store how often each entry occured
+ * in histogram.  Store the length of runs of UNASSIGNED in runs,
+ * reallocating the array as needed.  Return the number of bytes read
+ * from pdbfile.
+ */
+static off_t
+gather_data(FILE *pdbfile, off_t histogram[PDB_HISTOGRAM_LEN], off_t **runs, size_t *n_runs)
+{
+	size_t runlen = 0;
+	off_t size = 0;
+	int c;
+
+	flockfile(pdbfile);
+
+	while (c = getc_unlocked(pdbfile), c != EOF) {
+		size++;
+		histogram[c]++;
+		if (c != UNREACHED) {
+			if (runlen >= *n_runs) {
+				*runs = realloc(*runs, (runlen + 1) * sizeof **runs);
+				assert(*runs != NULL);
+				memset(*runs + *n_runs, 0, (runlen + 1 - *n_runs) * sizeof **runs);
+				*n_runs = runlen + 1;
+			}
+
+			(*runs)[runlen]++;
+			runlen = 0;
+		} else
+			runlen++;
+	}
+
+	funlockfile(pdbfile);
+	fclose(pdbfile);
+	return (size);
 }
 
 /*
@@ -26,13 +61,12 @@ usage(const char *argv0)
  * PDB and print it.
  */
 static void
-do_histogram(patterndb pdb, tileset ts, cmbindex histogram[PDB_HISTOGRAM_LEN])
+print_histogram(off_t histogram[PDB_HISTOGRAM_LEN], off_t size)
 {
 	size_t i;
-	double quotient = 1.0 / (double)search_space_size(ts), entropy, bits, prob, accum = 0.0;
+	double quotient = 1.0 / (double)size, entropy, bits, prob, accum = 0.0;
 
 	printf("histogram:\n");
-	generate_pdb_histogram(histogram, pdb, ts);
 
 	for (i = 0; i < PDB_HISTOGRAM_LEN; i++) {
 		if (histogram[i] == 0)
@@ -44,7 +78,7 @@ do_histogram(patterndb pdb, tileset ts, cmbindex histogram[PDB_HISTOGRAM_LEN])
 		accum += bits;
 
 		printf("0x%02zx: %20llu * %6.2fb (%6.2f%%) = %23.2fb (%23.2fB)\n",
-		    i, histogram[i], entropy, 100.0 * prob, bits, bits / 8);
+		    i, (unsigned long long)histogram[i], entropy, 100.0 * prob, bits, bits / 8);
 	}
 
 	printf("total %.2fb (%.2fB)\n\n", accum, accum / 8);
@@ -56,33 +90,12 @@ do_histogram(patterndb pdb, tileset ts, cmbindex histogram[PDB_HISTOGRAM_LEN])
  * would be.  Do nothing if no UNREACHED entries occur.
  */
 static void
-do_runs(patterndb pdb, tileset ts, cmbindex histogram[PDB_HISTOGRAM_LEN])
+print_runs(off_t histogram[PDB_HISTOGRAM_LEN], size_t n_pdb, off_t runs[], size_t n_runs)
 {
-	size_t i, n_pdb = search_space_size(ts), runlen = 0;
-	size_t *runs = NULL, n_runs = 0, run_count = 0;
+	size_t i, run_count = 0;
 	double quotient, entropy, bits, prob, run_accum = 0.0, hist_accum = 0.0;
 
-	if (histogram[UNREACHED] == 0) {
-		printf("No UNREACHED entries, skipping run length analysis.\n");
-		return;
-	}
-
 	printf("run-lengths:\n");
-
-	for (i = 0; i < n_pdb; i++) {
-		if (pdb[i] != UNREACHED) {
-			if (runlen >= n_runs) {
-				runs = realloc(runs, (i + 1) * sizeof *runs);
-				assert(runs != NULL);
-				memset(runs + n_runs, 0, (i + 1 - n_runs) * sizeof *runs);
-				n_runs = i + 1;
-			}
-
-			runs[runlen]++;
-			runlen = 0;
-		} else
-			runlen++;
-	}
 
 	for (i = 0; i < n_runs; i++)
 		run_count += runs[i];
@@ -114,7 +127,7 @@ do_runs(patterndb pdb, tileset ts, cmbindex histogram[PDB_HISTOGRAM_LEN])
 		hist_accum += bits;
 
 		printf("0x%02zx: %20llu * %6.2fb (%6.2f%%) = %23.2fb (%23.2fB)\n",
-		    i, histogram[i], entropy, 100.0 * prob, bits, bits / 8);
+		    i, (unsigned long long)histogram[i], entropy, 100.0 * prob, bits, bits / 8);
 	}
 
 	printf("total %.2fb (%.2fB)\n\n", hist_accum, hist_accum / 8);
@@ -126,81 +139,25 @@ do_runs(patterndb pdb, tileset ts, cmbindex histogram[PDB_HISTOGRAM_LEN])
 extern int
 main(int argc, char *argv[])
 {
-	tileset ts = 0x00000e7; /* 0 1 2 5 6 7 */
-	size_t count, size;
-	int optchar;
-	const char *fname = NULL;
-	FILE *f;
-	patterndb pdb;
-	cmbindex histogram[PDB_HISTOGRAM_LEN];
+	FILE *pdbfile;
+	off_t *runs = NULL, size, histogram[PDB_HISTOGRAM_LEN] = {};
+	size_t n_runs = 0;
 
-	while (optchar = getopt(argc, argv, "f:ij:o:t:"), optchar != -1)
-		switch (optchar) {
-		case 'f':
-			fname = optarg;
-			break;
+	if (argc != 2)
+	usage(argv[0]);
 
-		case 'j':
-			pdb_jobs = atoi(optarg);
-			if (pdb_jobs < 1 || pdb_jobs > PDB_MAX_JOBS) {
-				fprintf(stderr, "Number of threads must be between 1 and %d\n",
-				    PDB_MAX_JOBS);
-				return (EXIT_FAILURE);
-			}
-
-			break;
-
-		case 't':
-			if (tileset_parse(&ts, optarg) != 0) {
-				fprintf(stderr, "Cannot parse tile set: %s\n", optarg);
-				return (EXIT_FAILURE);
-			}
-
-			break;
-
-		case '?':
-		case ':':
-			usage(argv[0]);
-		}
-
-	if (tileset_count(ts) >= 16) {
-		fprintf(stderr, "%d tiles are too many tiles. Up to 15 tiles allowed.\n",
-		    tileset_count(ts));
-		return (EXIT_FAILURE);
-	}
-
-	if (fname == NULL)
-		usage(argv[0]);
-
-
-	f = fopen(fname, "rb");
-	if (f == NULL) {
+	pdbfile = fopen(argv[1], "rb");
+	if (pdbfile == NULL) {
 		perror("fopen");
 		return (EXIT_FAILURE);
 	}
 
-	size = search_space_size(ts);
 
-	pdb = malloc(size);
-	if (pdb == NULL) {
-		perror("malloc");
-		return (EXIT_FAILURE);
-	}
-
-	count = fread(pdb, 1, size, f);
-	if (count < size) {
-		if (ferror(f))
-			perror("fread");
-		else
-			fprintf(stderr, "PDB too short.\n");
-
-		return (EXIT_FAILURE);
-	}
-
+	size = gather_data(pdbfile, histogram, &runs, &n_runs);
 	printf("size %zuB\n\n", size);
-
-	do_histogram(pdb, ts, histogram);
-	do_runs(pdb, ts, histogram);
+	print_histogram(histogram, size);
+	if (histogram[UNREACHED] != 0)
+		print_runs(histogram, size, runs, n_runs);
 
 	return (EXIT_SUCCESS);
 }
