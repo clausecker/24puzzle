0bc70ea3417619d47907266a4219bfb75db69c0a
Robert Clausecker <fuz@fuz.su>
2017-09-12 17:20:52 +0200

    puzzle.c: Move movetab to moves.c where it logically belongs

--- moves.c
+++ moves.c
@@ -2,7 +2,45 @@
 
 #include <assert.h>
 
+#include "puzzle.h"
 #include "tileset.h"
+
+/*
+ * List of all possible moves for a given position of the empty square.
+ * There are up to four moves from every square, if there are less, the
+ * remainder is filled up with -1.
+ */
+const signed char movetab[TILE_COUNT][4] = {
+	 1,  5, -1, -1,
+	 0,  2,  6, -1,
+	 1,  3,  7, -1,
+	 2,  4,  8, -1,
+	 3,  9, -1, -1,
+
+	 0,  6, 10, -1,
+	 1,  5,  7, 11,
+	 2,  6,  8, 12,
+	 3,  7,  9, 13,
+	 4,  8, 14, -1,
+
+	 5, 11, 15, -1,
+	 6, 10, 12, 16,
+	 7, 11, 13, 17,
+	 8, 12, 14, 18,
+	 9, 13, 19, -1,
+
+	10, 16, 20, -1,
+	11, 15, 17, 21,
+	12, 16, 18, 22,
+	13, 17, 19, 23,
+	14, 18, 24, -1,
+
+	15, 21, -1, -1,
+	16, 20, 22, -1,
+	17, 21, 23, -1,
+	18, 22, 24, -1,
+	19, 23, -1, -1,
+};
 
 /*
  * Generate all moves that lead from any partial puzzle configuration
