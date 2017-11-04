clausecker/24puzzle

commit 802ef0eeff42c30159b04045f61b6bccbfd1b0a4

pdb.c: Simplify PDB structure
We can allocate the whole PDB in one chunk, this saves one read from poorly
cached memory for an extra (cheap) multiplication.  Sadly, pdb_ident() is
a bit more difficult to implement using this new scheme, so it is removed
for the time being.  The PDB layout has not changed, so old identified PDBs
can still be used.

pdb.h
@@ -12,14 +12,14 @@
  * A patterndb is an array of pointers to arrays of bytes
  * representing the distance from the represented partial puzzle
  * configuration to the solved puzzle.  The member aux describes the
- * tile set we use to compute indices.  The tables are organized first
- * by map rank, then by permutation index, and finally by equivalence
- * class.
+ * tile set we use to compute indices.  data points to the content of
+ * the PDB, organized first by map rank, then by permutation index,
+ * and finally by equivalence class.
  */
 struct patterndb {
 	struct index_aux aux;
 	int mapped; /* true if PDB has been allocated using mmap() */
-	atomic_uchar *tables[];
+	atomic_uchar *data;
 };
 
 _Static_assert(sizeof(atomic_uchar) == 1, "Machine does not support proper atomic chars.");
@@ -74,7 +74,6 @@ extern int	pdb_verify(struct patterndb *, FILE *);
 extern int	pdb_histogram(size_t[PDB_HISTOGRAM_LEN], struct patterndb *);
 extern void	pdb_reduce(struct patterndb *, FILE *);
 extern void	pdb_diffcode(struct patterndb *, unsigned char[]);
-extern void	pdb_identify(struct patterndb *);
 
 /*
  * Return a pointer to the PDB entry for idx.
@@ -83,9 +82,9 @@ static inline atomic_uchar *
 pdb_entry_pointer(struct patterndb *pdb, const struct index *idx)
 {
 	if (tileset_has(pdb->aux.ts, ZERO_TILE))
-		return (pdb->tables[idx->maprank] + idx->eqidx * pdb->aux.n_perm + idx->pidx);
+		return (pdb->data + (pdb->aux.idxt[idx->maprank].offset + idx->eqidx) * pdb->aux.n_perm + idx->pidx);
 	else
-		return (pdb->tables[idx->maprank] + idx->pidx);
+		return (pdb->data + idx->maprank * pdb->aux.n_perm + idx->pidx);
 }
 
 /*

Makefile
@@ -5,7 +5,7 @@ LDLIBS=-lpthread
 
 OBJ=index.o puzzle.o tileset.o validation.o ranktbl.o rank.o random.o pdb.o \
 	moves.o parallel.o pdbgen.o pdbverify.o pdbdiff.o histogram.o \
-	cindex.o pdbdom.o pdbident.o ida.o search.o catalogue.o
+	cindex.o pdbdom.o ida.o search.o catalogue.o
 BINARIES=cmd/pdbstats test/indextest util/rankgen test/ranktest \
 	cmd/genpdb cmd/verifypdb cmd/diffcode cmd/reducepdb test/qualitytest \
 	cmd/pdbsearch test/hitanalysis

cmd/genpdb.c
@@ -13,7 +13,7 @@
 static void
 usage(const char *argv0)
 {
-	fprintf(stderr, "Usage: %s [-i] [-f file] [-t tile,tile,...] [-j nproc]\n", argv0);
+	fprintf(stderr, "Usage: %s [-f file] [-t tile,tile,...] [-j nproc]\n", argv0);
 
 	exit(EXIT_FAILURE);
 }
@@ -23,18 +23,14 @@ main(int argc, char *argv[])
 {
 	struct patterndb *pdb;
 	tileset ts = DEFAULT_TILESET;
-	int optchar, identify = 0;
+	int optchar;
 	const char *fname = NULL;
 	FILE *f = NULL;
 
-	while (optchar = getopt(argc, argv, "f:ij:t:"), optchar != -1)
+	while (optchar = getopt(argc, argv, "f:j:t:"), optchar != -1)
 		switch (optchar) {
 		case 'f':
 			fname = optarg;
-			break;
-
-		case 'i':
-			identify = 1;
 			break;
 
 		case 'j':
@@ -66,9 +62,6 @@ main(int argc, char *argv[])
 		return (EXIT_FAILURE);
 	}
 
-	if (identify)
-		ts = tileset_add(ts, ZERO_TILE);
-
 	if (fname != NULL) {
 		f = fopen(fname, "wb");
 		if (f == NULL) {
@@ -84,10 +77,6 @@ main(int argc, char *argv[])
 	}
 
 	pdb_generate(pdb, stderr);
-	if (identify) {
-		fprintf(stderr, "Identifying PDB...\n");
-		pdb_identify(pdb);
-	}
 
 	if (f != NULL && pdb_store(f, pdb) != 0) {
 		perror("pdb_store");

histogram.c
@@ -24,11 +24,12 @@ histogram_worker(void *cfgarg, struct index *idx)
 	struct histogram_config *cfg = cfgarg;
 	size_t i, n = pdb_table_size(cfg->pcfg.pdb, idx->maprank);
 	size_t histogram[PDB_HISTOGRAM_LEN];
+	atomic_uchar *table = pdb_entry_pointer(cfg->pcfg.pdb, idx);
 
 	memset(histogram, 0, sizeof histogram);
 
 	for (i = 0; i < n; i++)
-		histogram[cfg->pcfg.pdb->tables[idx->maprank][i]]++;
+		histogram[table[i]]++;
 
 	for (i = 0; i < PDB_HISTOGRAM_LEN; i++)
 		cfg->histogram[i] += histogram[i];

pdb.c
@@ -21,27 +21,21 @@
 extern struct patterndb *
 pdb_allocate(tileset ts)
 {
-	size_t i;
 	struct index_aux aux;
 	struct patterndb *pdb;
 
 	make_index_aux(&aux, ts);
-	pdb = malloc(sizeof *pdb + sizeof *pdb->tables * aux.n_maprank);
+	pdb = malloc(sizeof *pdb);
 	if (pdb == NULL)
 		return (NULL);
 
 	pdb->aux = aux;
 	pdb->mapped = 0;
 
-	/* allow us to simply call pdb_free() if we run out of memory */
-	memset(pdb->tables, 0, sizeof *pdb->tables * aux.n_maprank);
-
-	for (i = 0; i < aux.n_maprank; i++) {
-		pdb->tables[i] = malloc(pdb_table_size(pdb, i));
-		if (pdb->tables[i] == NULL) {
-			pdb_free(pdb);
-			return (NULL);
-		}
+	pdb->data = malloc(search_space_size(&pdb->aux));
+	if (pdb->data == NULL) {
+		free(pdb);
+		return (NULL);
 	}
 
 	return (pdb);
@@ -53,13 +47,11 @@ pdb_allocate(tileset ts)
 extern void
 pdb_free(struct patterndb *pdb)
 {
-	size_t i, n_tables = pdb->aux.n_maprank;
 
 	if (pdb->mapped)
-		munmap(pdb->tables[0], search_space_size(&pdb->aux));
+		munmap(pdb->data, search_space_size(&pdb->aux));
 	else
-		for (i = 0; i < n_tables; i++)
-			free(pdb->tables[i]);
+		free(pdb->data);
 
 	free(pdb);
 }
@@ -70,11 +62,8 @@ pdb_free(struct patterndb *pdb)
 extern void
 pdb_clear(struct patterndb *pdb)
 {
-	size_t i, n_tables = pdb->aux.n_maprank;
 
-	for (i = 0; i < n_tables; i++) {
-		memset((void *)pdb->tables[i], UNREACHED, pdb_table_size(pdb, i));
-	}
+	memset((void*)pdb->data, UNREACHED, search_space_size(&pdb->aux));
 }
 
 /*
@@ -89,22 +78,19 @@ extern struct patterndb *
 pdb_load(tileset ts, FILE *pdbfile)
 {
 	struct patterndb *pdb = pdb_allocate(ts);
-	size_t i, n_tables = pdb->aux.n_maprank, count, tblsize;
+	size_t count, size = search_space_size(&pdb->aux);
 
 	if (pdb == NULL)
 		return (NULL);
 
-	for (i = 0; i < n_tables; i++) {
-		tblsize = pdb_table_size(pdb, i);
-		count = fread((void *)pdb->tables[i], 1, tblsize, pdbfile);
-		if (count != tblsize) {
-			/* tell apart short read from IO error */
-			if (!ferror(pdbfile))
-				errno = EINVAL;
+	count = fread((void *)pdb->data, 1, size, pdbfile);
+	if (count != size) {
+		/* tell apart short read from IO error */
+		if (!ferror(pdbfile))
+			errno = EINVAL;
 
-			pdb_free(pdb);
-			return (NULL);
-		}
+		pdb_free(pdb);
+		return (NULL);
 	}
 
 	return (pdb);
@@ -119,18 +105,15 @@ pdb_load(tileset ts, FILE *pdbfile)
 extern int
 pdb_store(FILE *pdbfile, struct patterndb *pdb)
 {
-	size_t i, n_tables = pdb->aux.n_maprank, count, tblsize;
+	size_t count, size = search_space_size(&pdb->aux);
 
-	for (i = 0; i < n_tables; i++) {
-		tblsize = pdb_table_size(pdb, i);
-		count = fwrite((void *)pdb->tables[i], 1, tblsize, pdbfile);
-		if (count != tblsize) {
-			/* tell apart end of medium from IO error */
-			if (!ferror(pdbfile))
-				errno = ENOSPC;
+	count = fwrite((void *)pdb->data, 1, size, pdbfile);
+	if (count != size) {
+		/* tell apart end of medium from IO error */
+		if (!ferror(pdbfile))
+			errno = ENOSPC;
 
-			return (-1);
-		}
+		return (-1);
 	}
 
 	return (0);
@@ -147,7 +130,6 @@ pdb_mmap(tileset ts, int pdbfd, int mapflags)
 {
 	struct index_aux aux;
 	struct patterndb *pdb;
-	size_t i, offset = 0;
 	int prot, flags;
 
 	switch (mapflags) {
@@ -172,22 +154,16 @@ pdb_mmap(tileset ts, int pdbfd, int mapflags)
 	}
 
 	make_index_aux(&aux, ts);
-	pdb = malloc(sizeof *pdb + sizeof *pdb->tables * aux.n_maprank);
+	pdb = malloc(sizeof *pdb);
 	if (pdb == NULL)
 		return (NULL);
 
 	pdb->aux = aux;
 	pdb->mapped = 1;
-	pdb->tables[0] = mmap(NULL, search_space_size(&pdb->aux), prot, flags, pdbfd, 0);
-	if (pdb->tables[0] == MAP_FAILED) {
-		pdb->tables[0] = NULL;
+	pdb->data = mmap(NULL, search_space_size(&pdb->aux), prot, flags, pdbfd, 0);
+	if (pdb->data == MAP_FAILED) {
 		free(pdb);
 		return (NULL);
-	}
-
-	for (i = 1; i < aux.n_maprank; i++) {
-		offset += pdb_table_size(pdb, i - 1);
-		pdb->tables[i] = pdb->tables[0] + offset;
 	}
 
 	return (pdb);

pdbident.c
@@ -1,68 +1,0 @@
-/* pdbident.c -- identify equivalence classes */
-
-#include <assert.h>
-#include <stdatomic.h>
-#include <stdlib.h>
-
-#include "parallel.h"
-#include "pdb.h"
-
-/*
- * Identify equivalence classes in the table for idx->maprank.
- * Afterwards, reallocate the table to reduce memory usage.
- */
-static void
-identify_worker(void *cfgarg, struct index *idx)
-{
-	struct parallel_config *cfg = cfgarg;
-	struct index_aux *aux = &cfg->pdb->aux;
-	size_t pidx, eqidx;
-	size_t n_eqclass = aux->idxt[idx->maprank].n_eqclass, n_perm = aux->n_perm;
-	int min, entry;
-	atomic_uchar *table = cfg->pdb->tables[idx->maprank], *new_table;
-
-	if (n_eqclass == 1)
-		return;
-
-	for (pidx = 0; pidx < n_perm; pidx++) {
-		min = UNREACHED;
-		for (eqidx = 0; eqidx < n_eqclass; eqidx++) {
-			entry = table[eqidx * n_perm + pidx];
-			if (entry < min)
-				min = entry;
-		}
-
-		table[pidx] = min;
-	}
-
-	new_table = realloc(cfg->pdb->tables[idx->maprank], n_perm);
-	if (new_table != NULL)
-		cfg->pdb->tables[idx->maprank] = new_table;
-}
-
-/*
- * Identify equivalence classes of a pattern database.  This function
- * merges all equivalence classes of a given map into one, keeping only
- * the entry with the lowest distance.  Effectively, this removes the
- * zero-awareness of a PDB but yields a better heuristic than just
- * generating such a PDB in the first place.  The PDB is modified
- * in-place.  The pdb must not be  memory-mapped.  This fucntion is a
- * no-op if pdb is zero-unaware.
- */
-extern void
-pdb_identify(struct patterndb *pdb)
-{
-	tileset ts;
-	struct parallel_config pcfg;
-
-	assert(pdb->mapped == 0);
-	if (!tileset_has(pdb->aux.ts, ZERO_TILE))
-		return;
-
-	pcfg.pdb = pdb;
-	pcfg.worker = identify_worker;
-
-	pdb_iterate_parallel(&pcfg);
-	ts = pdb->aux.ts;
-	make_index_aux(&pdb->aux, tileset_remove(ts, ZERO_TILE));
-}

commit 18123a92ccf4c1fbd6a296e85b6556d83ff0ed78

pdb.h: Fix integer overflow in pdb_entry_pointer()
This is an accidental regression introduced in revision
802ef0eeff42c30159b04045f61b6bccbfd1b0a4 "pdb.c: Simplify PDB structure"
This regression wasn't spotted before as I rarely had a need to use
large pattern databases.

pdb.h
@@ -108,9 +108,11 @@ static inline atomic_uchar *
 pdb_entry_pointer(struct patterndb *pdb, const struct index *idx)
 {
 	if (tileset_has(pdb->aux.ts, ZERO_TILE))
-		return (pdb->data + (pdb->aux.idxt[idx->maprank].offset + idx->eqidx) * pdb->aux.n_perm + idx->pidx);
+		return (pdb->data + (pdb->aux.idxt[idx->maprank].offset +
+		    idx->eqidx) * (size_t)pdb->aux.n_perm + idx->pidx);
 	else
-		return (pdb->data + idx->maprank * pdb->aux.n_perm + idx->pidx);
+		return (pdb->data + idx->maprank * (size_t)pdb->aux.n_perm +
+		    idx->pidx);
 }
 
 /*