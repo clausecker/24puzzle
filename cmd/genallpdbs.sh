7a4a7603792fc08d1f6c296498b7d37dcb6875fd
Robert Clausecker <fuz@fuz.su>
2017-11-20 18:25:30 +0100

    cmd: Add script cmd/genallpdbs.sh

---
+++ cmd/genallpdbs.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+set -e
+
+# this script generates all PDBs of the given size, 6 by default and
+# stores them to the given directory.
+tiles=6
+pdbdir=/local/bzcclaus/scratch/pdbs
+cmddir=.
+datefmt="%Y-%m-%d %T:"
+
+PATH="$cmddir:$PATH"
+
+# are we a worker?
+if [ "$1" = -w ]
+then
+	ts="$2"
+	pdbstem="$pdbdir/$ts"
+
+	# has this already been generated?
+	if [ -e "$pdbstem.bpdb.zst" ] && zstd -qq -t "$pdbstem.bpdb.zst"
+	then
+		echo `date "+$datefmt"` "$ts" already present
+		exit
+	fi
+
+	genpdb -q -j 1 -t "$ts" -f "$pdbstem.pdb"
+	bitpdb -t "$ts" -o "$pdbstem.bpdb" "$pdbstem.pdb"
+	rm -f "$pdbstem.pdb"
+	zstd -q --rm --ultra -22 -f "$pdbstem.bpdb"
+	echo `date "+$datefmt"` "$ts" generated
+	exit
+fi
+
+# find number of CPUs to use
+if [ ! "$NPROC" ]
+then
+	case `uname` in
+	Linux) NPROC=`nproc` ;;
+	Darwin|FreeBSD) NPROC=`sysctl -n hw.ncpu` ;;
+	*) NPROC=1 ;;
+	esac
+fi
+
+echo `date "+$datefmt"` Generating all PDBs for "$tiles" tiles to "$pdbdir"
+mkdir -p "$pdbdir"
+pdbcount -z -p "$tiles" | xargs -P "$NPROC" -n 1 -- "$0" -w
+echo `date "+$datefmt"` Finished generating all PDBs for "$tiles" tiles to $pdbdir"
