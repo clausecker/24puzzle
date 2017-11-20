51c4d10184712e1ed33e273fba113652aee26a52
Robert Clausecker <fuz@fuz.su>
2017-11-20 17:27:15 +0100

    test/pdbcount.c: Add options -a, -p, and -z

--- test/pdbcount.c
+++ test/pdbcount.c
@@ -25,8 +25,10 @@
 
 /* pdbcount.c -- count the number of truly distinct PDBs */
 
+#define _POSIX_C_SOURCE 200809L
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #include "tileset.h"
 #include "transposition.h"
@@ -63,47 +65,86 @@ static const unsigned pdbcount[25] = {
 /*
  * Count how many unique PDBs with n_tiles tiles exist.  If zero is 0,
  * do not account for the zero tile.  If zero is 1, do account for the
- * zero tile.
+ * zero tile.  If print is 0, print statistics at the end.  If print is
+ * 1, instead print each canonical PDB.
  */
 static void
-count_pdbs(int n_tiles, int zero)
+count_pdbs(int n_tiles, int zero, int print)
 {
 	size_t i, n = pdbcount[n_tiles], c = 0;
 	tileset t = tileset_least(n_tiles), ts;
+	char buf[TILESET_LIST_LEN];
 
 	for (i = 0; i < n; i++) {
 		ts = t << 1 | zero;
-		if (canonical_automorphism(ts) == 0)
+		if (canonical_automorphism(ts) == 0) {
 			c++;
+			if (print) {
+				tileset_list_string(buf, ts);
+				puts(buf);
+			}
+		}
 
 		t = next_combination(t);
 	}
+
+	if (print)
+		return;
 
 	printf("%s: %20zu / %20zu (%5.2f%%)\n", zero ? "ZPDB" : "APDB",
 	    c, (size_t)pdbcount[n_tiles],
 	    (100.0 * c) / pdbcount[n_tiles]);
 }
 
+static void
+usage(const char *argv0)
+{
+	fprintf(stderr, "Usage: %s [-azp] [n_tiles]\n", argv0);
+	exit(EXIT_FAILURE);
+}
+
 extern int
 main(int argc, char *argv[])
 {
-	int n_tiles = 6;
+	int n_tiles = 6, optchar;
+	int print = 0, do_apdb = 1, do_zpdb = 1;
 
-	switch (argc) {
+	while (optchar = getopt(argc, argv, "apz"), optchar != -1)
+		switch (optchar) {
+		case 'a':
+			do_zpdb = 0;
+			break;
+
+		case 'z':
+			do_apdb = 0;
+			break;
+
+		case 'p':
+			print = 1;
+			break;
+
+		case ':':
+		case '?':
+			usage(argv[0]);
+		}
+
+	switch (argc - optind) {
 	case 0:
-	case 1:
 		break;
 
-	case 2:
-		n_tiles = atoi(argv[1]);
+	case 1:
+		n_tiles = atoi(argv[optind]);
 		break;
 
 	default:
-		fprintf(stderr, "Usage: %s [n_tiles]\n", argv[0]);
+		usage(argv[0]);
 	}
 
-	count_pdbs(n_tiles, 0);
-	count_pdbs(n_tiles, 1);
+	if (do_apdb)
+		count_pdbs(n_tiles, 0, print);
+
+	if (do_zpdb)
+		count_pdbs(n_tiles, 1, print);
 
 	return (EXIT_SUCCESS);
 }
