b218ac090ca3b93fad482470b141a2eaef038634
Robert Clausecker <fuz@fuz.su>
2018-01-15 14:18:28 +0100

    Update copyright year in files updated in 2018

--- ida.c
+++ ida.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2017 Robert Clausecker. All rights reserved.
+ * Copyright (c) 2017--2018 Robert Clausecker. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
